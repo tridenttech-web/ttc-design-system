6bc2be2446567c5a9e0a534bcdc658cc8e36437c
Daniel Vice <trident.mcmaster@gmail.com>
2026-06-11 15:57:33 -0400

    Changed the primary button color from primary to secondary.

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -3,7 +3,7 @@ export default {
 };
 
 export const Primary = () => `
-  <a href="#" class="inline-flex items-center rounded-md bg-brand-primary px-4 py-2 font-semibold text-white no-underline">
+  <a href="#" class="inline-flex items-center rounded-md bg-brand-secondary px-4 py-2 font-semibold text-white no-underline">
     Apply now
   </a>
 `;
